8348b7045713950a1ecb6f8df2d2561f23f24160
wilywangceo@gmail.com <wilywangceo@gmail.com>
2024-07-11 18:36:14 +0700

    add logo

--- src/components/layout/about/Skill.jsx
+++ src/components/layout/about/Skill.jsx
@@ -5,214 +5,164 @@ import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
 const Skill = () => {
-    const [item, inView] = useInView({
-        triggerOnce: true,
-    });
+  const [item, inView] = useInView({
+    triggerOnce: true,
+  });
 
-    const ContainerVariants = {
-        hidden: {},
-        visible: {
-            transition: {
-                staggerChildren: 0.3,
-            },
-        },
-    };
+  const ContainerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.3,
+      },
+    },
+  };
 
-    const ChildrenVariants = {
-        hidden: {
-            y: 100,
-            opacity: 0,
-        },
-        visible: {
-            y: 0,
-            opacity: 100,
-        },
-    };
+  const ChildrenVariants = {
+    hidden: {
+      y: 100,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 100,
+    },
+  };
 
-    return (
-        <motion.div
-            className='grid grid-cols-2 gap-x-10 gap-y-5 py-16 md:grid-cols-3 xl:grid-cols-4'
-            variants={ContainerVariants}
-            ref={item}
-            initial='hidden'
-            animate={inView ? 'visible' : 'hidden'}
-        >
-            <motion.div variants={ChildrenVariants}>
-                <img src={image.Html} className=' w-10 pb-1' alt='Image-Icon' />
-                <p className='text-gray-400 text-sm md:text-base'>
-                    <span
-                        className='font-extrabold text-xl text-black'
-                        id='skill-header'
-                    >
-                        HTML
-                    </span>
-                    <br />
-                    Hypertext Markup Language is the standard markup language
-                    for documents designed to be displayed in internet browsers
-                </p>
-            </motion.div>
-            <motion.div variants={ChildrenVariants}>
-                <img src={image.CSS} alt='Image-Icon' className='w-10 pb-1' />
-                <p className='text-gray-400 text-sm md:text-base'>
-                    <span
-                        className='font-extrabold text-xl text-black'
-                        id='skill-header'
-                    >
-                        CSS
-                    </span>
-                    <br />
-                    Cascading Style Sheets are Sheets rules form managing
-                    several componenets in a web so that it will be more
-                    structured and uniform
-                </p>
-            </motion.div>
-            <motion.div variants={ChildrenVariants}>
-                <img src={image.Js} alt='Image-Icon' className='w-10 pb-1' />
-                <p className='text-gray-400 text-sm md:text-base'>
-                    <span
-                        className='font-extrabold text-xl text-black'
-                        id='skill-header'
-                    >
-                        Javascript
-                    </span>
-                    <br />
-                    Javascript is a high-level end dynamic programming language
-                </p>
-            </motion.div>
-            <motion.div variants={ChildrenVariants}>
-                <img
-                    src={image.Tailwind}
-                    alt='Image-Icon'
-                    className='w-10 pb-2'
-                />
-                <span
-                    className='font-extrabold text-xl text-black'
-                    id='skill-header'
-                >
-                    TailwindCSS
-                </span>
-                <br />
-                <p className='text-gray-400 text-sm md:text-base'>
-                    A utulity-first CSS frameworks packed with classes like
-                    flex, pt-8, text-center, and rotate-45 than can be composed
-                    to build any design, directly in your markup
-                </p>
-            </motion.div>
-            <motion.div variants={ChildrenVariants}>
-                <img src={image.Typ} alt='Image-Icon' className='w-10 pb-2' />
-                <p className='text-gray-400 text-sm md:text-base'>
-                    <span
-                        className='font-extrabold text-xl text-black'
-                        id='skill-header'
-                    >
-                        Typescript
-                    </span>
-                    <br />
-                    Typescript is a strongly typed programming language thats
-                    build on Javascript, giving you better tooling at any scale
-                </p>
-            </motion.div>
-            <motion.div variants={ChildrenVariants}>
-                <img src={image.SCSS} alt='Image-Icon' className='w-10 pb-1' />
-                <p className='text-gray-400 text-sm md:text-base'>
-                    <span
-                        className='font-extrabold text-xl text-black'
-                        id='skill-header'
-                    >
-                        Sass
-                    </span>{' '}
-                    <br />
-                    Sass is the most mature, stable, and powerful professional
-                    grade CSS extension language in the world
-                </p>
-            </motion.div>
-            <motion.div variants={ChildrenVariants}>
-                <img src={image.React} alt='Image-Icon' className='w-10 pb-1' />
-                <p className='text-gray-400 text-sm md:text-base'>
-                    <span
-                        className='font-extrabold text-xl text-black'
-                        id='skill-header'
-                    >
-                        React
-                    </span>{' '}
-                    <br />
-                    React is a javascript library used to build component-based
-                    interactive user interfaces
-                </p>
-            </motion.div>
-            <motion.div variants={ChildrenVariants}>
-                <img src={image.Node} alt='Image-Icon' className='w-10 pb-1' />
-                <p className='text-gray-400 text-sm md:text-base'>
-                    <span
-                        className='font-extrabold text-xl text-black'
-                        id='skill-header'
-                    >
-                        Node
-                    </span>{' '}
-                    <br />
-                    Node.js is a runtime environment for Javascript that is
-                    open-source and cross platform
-                </p>
-            </motion.div>
-            <motion.div variants={ChildrenVariants}>
-                <img
-                    src={image.Mongodb}
-                    alt='Image-Icon'
-                    className='w-10 pb-1'
-                />
-                <p className='text-gray-400 text-sm md:text-base'>
-                    <span
-                        className='font-extrabold text-xl text-black'
-                        id='skill-header'
-                    >
-                        PostgresSql
-                    </span>{' '}
-                    <br />
-                    Next.js is a popular open-source React framework for
-                    building web applications. it allow developers to build
-                    modern, server-render React applications with ease
-                </p>
-            </motion.div>
-            <motion.div variants={ChildrenVariants}>
-                <img
-                    src={image.Mongodb}
-                    alt='Image-Icon'
-                    className='w-10 pb-1'
-                />
-                <p className='text-gray-400 text-sm md:text-base'>
-                    <span
-                        className='font-extrabold text-xl text-black'
-                        id='skill-header'
-                    >
-                        MongoDB
-                    </span>{' '}
-                    <br />
-                    Next.js is a popular open-source React framework for
-                    building web applications. it allow developers to build
-                    modern, server-render React applications with ease
-                </p>
-            </motion.div>
-            <motion.div variants={ChildrenVariants}>
-                <img
-                    src={image.Express}
-                    alt='Image-Icon'
-                    className='w-10 pb-1'
-                />
-                <p className='text-gray-400 text-sm md:text-base'>
-                    <span
-                        className='font-extrabold text-xl text-black'
-                        id='skill-header'
-                    >
-                        Express
-                    </span>{' '}
-                    <br />
-                    Next.js is a popular open-source React framework for
-                    building web applications. it allow developers to build
-                    modern, server-render React applications with ease
-                </p>
-            </motion.div>
-        </motion.div>
-    );
+  return (
+    <motion.div
+      className='grid grid-cols-2 gap-x-10 gap-y-5 py-16 md:grid-cols-3 xl:grid-cols-4'
+      variants={ContainerVariants}
+      ref={item}
+      initial='hidden'
+      animate={inView ? 'visible' : 'hidden'}
+    >
+      <motion.div variants={ChildrenVariants}>
+        <img src={image.Html} className=' w-10 pb-1' alt='Image-Icon' />
+        <p className='text-gray-400 text-sm md:text-base'>
+          <span className='font-extrabold text-xl text-black' id='skill-header'>
+            HTML
+          </span>
+          <br />
+          Hypertext Markup Language is the standard markup language for
+          documents designed to be displayed in internet browsers
+        </p>
+      </motion.div>
+      <motion.div variants={ChildrenVariants}>
+        <img src={image.CSS} alt='Image-Icon' className='w-10 pb-1' />
+        <p className='text-gray-400 text-sm md:text-base'>
+          <span className='font-extrabold text-xl text-black' id='skill-header'>
+            CSS
+          </span>
+          <br />
+          Cascading Style Sheets are Sheets rules form managing several
+          componenets in a web so that it will be more structured and uniform
+        </p>
+      </motion.div>
+      <motion.div variants={ChildrenVariants}>
+        <img src={image.Js} alt='Image-Icon' className='w-10 pb-1' />
+        <p className='text-gray-400 text-sm md:text-base'>
+          <span className='font-extrabold text-xl text-black' id='skill-header'>
+            Javascript
+          </span>
+          <br />
+          Javascript is a high-level end dynamic programming language
+        </p>
+      </motion.div>
+      <motion.div variants={ChildrenVariants}>
+        <img src={image.Tailwind} alt='Image-Icon' className='w-10 pb-2' />
+        <span className='font-extrabold text-xl text-black' id='skill-header'>
+          TailwindCSS
+        </span>
+        <br />
+        <p className='text-gray-400 text-sm md:text-base'>
+          A utulity-first CSS frameworks packed with classes like flex, pt-8,
+          text-center, and rotate-45 than can be composed to build any design,
+          directly in your markup
+        </p>
+      </motion.div>
+      <motion.div variants={ChildrenVariants}>
+        <img src={image.Typ} alt='Image-Icon' className='w-10 pb-2' />
+        <p className='text-gray-400 text-sm md:text-base'>
+          <span className='font-extrabold text-xl text-black' id='skill-header'>
+            Typescript
+          </span>
+          <br />
+          Typescript is a strongly typed programming language thats build on
+          Javascript, giving you better tooling at any scale
+        </p>
+      </motion.div>
+      <motion.div variants={ChildrenVariants}>
+        <img src={image.SCSS} alt='Image-Icon' className='w-10 pb-1' />
+        <p className='text-gray-400 text-sm md:text-base'>
+          <span className='font-extrabold text-xl text-black' id='skill-header'>
+            Sass
+          </span>{' '}
+          <br />
+          Sass is the most mature, stable, and powerful professional grade CSS
+          extension language in the world
+        </p>
+      </motion.div>
+      <motion.div variants={ChildrenVariants}>
+        <img src={image.React} alt='Image-Icon' className='w-10 pb-1' />
+        <p className='text-gray-400 text-sm md:text-base'>
+          <span className='font-extrabold text-xl text-black' id='skill-header'>
+            React
+          </span>{' '}
+          <br />
+          React is a javascript library used to build component-based
+          interactive user interfaces
+        </p>
+      </motion.div>
+      <motion.div variants={ChildrenVariants}>
+        <img src={image.Node} alt='Image-Icon' className='w-10 pb-1' />
+        <p className='text-gray-400 text-sm md:text-base'>
+          <span className='font-extrabold text-xl text-black' id='skill-header'>
+            Node
+          </span>{' '}
+          <br />
+          Node.js is a runtime environment for Javascript that is open-source
+          and cross platform
+        </p>
+      </motion.div>
+      <motion.div variants={ChildrenVariants}>
+        <img src={image.psql} alt='Image-Icon' className='w-10 pb-1' />
+        <p className='text-gray-400 text-sm md:text-base'>
+          <span className='font-extrabold text-xl text-black' id='skill-header'>
+            PostgresSql
+          </span>{' '}
+          PostgreSQL is a powerful, open-source relational database system. It
+          is known for its robustness, support for complex queries, and advanced
+          features like transactional integrity and concurrency control.
+        </p>
+      </motion.div>
+      <motion.div variants={ChildrenVariants}>
+        <img src={image.Mongodb} alt='Image-Icon' className='w-10 pb-1' />
+        <p className='text-gray-400 text-sm md:text-base'>
+          <span className='font-extrabold text-xl text-black' id='skill-header'>
+            MongoDB
+          </span>{' '}
+          <br />
+          MongoDB is a NoSQL database known for its flexibility and scalability.
+          It stores data in a JSON-like format called BSON (Binary JSON), which
+          makes it easy to work with complex and hierarchical data structures.
+        </p>
+      </motion.div>
+      <motion.div variants={ChildrenVariants}>
+        <img src={image.Express} alt='Image-Icon' className='w-10 pb-1' />
+        <p className='text-gray-400 text-sm md:text-base'>
+          <span className='font-extrabold text-xl text-black' id='skill-header'>
+            Express
+          </span>{' '}
+          <br />
+          Express is a minimal and flexible Node.js web application framework.
+          It provides a robust set of features to build web and mobile
+          applications, including routing, middleware, and HTTP utility methods
+          for creating APIs.
+        </p>
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default Skill;
